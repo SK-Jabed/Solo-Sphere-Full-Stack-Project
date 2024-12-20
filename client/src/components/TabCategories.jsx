12fa29c7da1e3c3fe0040dcd5a0a327a3ae6f844
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-20 23:55:03 +0600

    Try, Catch Functionality Added to Add Jobs Page

--- client/src/components/TabCategories.jsx
+++ client/src/components/TabCategories.jsx
@@ -16,6 +16,7 @@ const TabCategories = () => {
   useEffect(() => {
     fetchAllJobs();
   }, []);
+
   return (
     <Tabs>
       <div className=" container px-6 py-10 mx-auto">
@@ -37,25 +38,31 @@ const TabCategories = () => {
         </div>
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs.map((job) => (
-              <JobCard key={job._id} job={job}></JobCard>
-            ))}
+            {jobs
+              .filter((job) => job.category === "Web Development")
+              .map((job) => (
+                <JobCard key={job._id} job={job}></JobCard>
+              ))}
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs.map((job) => (
-              <JobCard key={job._id} job={job}></JobCard>
-            ))}
+            {jobs
+              .filter((job) => job.category === "Graphics Design")
+              .map((job) => (
+                <JobCard key={job._id} job={job}></JobCard>
+              ))}
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs.map((job) => (
-              <JobCard key={job._id} job={job}></JobCard>
-            ))}
+            {jobs
+              .filter((job) => job.category === "Digital Marketing")
+              .map((job) => (
+                <JobCard key={job._id} job={job}></JobCard>
+              ))}
           </div>
         </TabPanel>
       </div>
@@ -63,4 +70,4 @@ const TabCategories = () => {
   );
 }
 
-export default TabCategories
+export default TabCategories;
