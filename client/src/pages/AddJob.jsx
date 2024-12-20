0168c7b22f649111ebc885f35994ad2c8d7cd7d8
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-20 18:12:07 +0600

    Add Jobs Pages Functionality Completed

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,9 +1,58 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import { AuthContext } from '../providers/AuthProvider'
+import axios from 'axios'
+// import Swal from "sweetalert2"
 
 const AddJob = () => {
-  const [startDate, setStartDate] = useState(new Date())
+  const { user } = useContext(AuthContext);
+  const [startDate, setStartDate] = useState(new Date());
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+
+    const form = e.target;
+    const title = form.job_title.value;
+    const email = form.email.value;
+    const deadline = startDate;
+    const category = form.category.value;
+    const minPrice = parseFloat(form.min_price.value);
+    const maxPrice = parseFloat(form.max_price.value);
+    const description = form.description.value;
+
+    const formData = {
+      title,
+      buyer: {
+        email,
+        name: user?.displayName,
+        photo: user?.photoURL
+      },
+      deadline,
+      category,
+      minPrice,
+      maxPrice,
+      description,
+      bidCount: 0
+    };
+    // Make a POST Request to Server
+    const { data } = await axios.post(
+      `${import.meta.env.VITE_API_URL}/add-job`,
+      formData
+    )
+    // .then((data) => {
+    //   if (data.insertedId) {
+    //     Swal.fire({
+    //       position: "center",
+    //       icon: "success",
+    //       title: "Job Has been added.",
+    //       showConfirmButton: false,
+    //       timer: 1500,
+    //     });
+    //   }
+    // })
+    console.log(data);
+  }
 
   return (
     <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
@@ -12,7 +61,7 @@ const AddJob = () => {
           Post a Job
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='job_title'>
@@ -34,6 +83,8 @@ const AddJob = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={user?.email}
+                readOnly
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
